d41707197d219dc80f0b89b68c88e302ce7ea7cc
Nathalia Lanzendorf <nathalia.lanzendorf@gmail.com>
2024-07-09 19:00:09 -0300

    Criada estrutura inicial do projeto BE

---
+++ src/main/java/sc/senai/futuro/dev/tubarao/lab/receitas/LabReceitasApplication.java
@@ -0,0 +1,13 @@
+package sc.senai.futuro.dev.tubarao.lab.receitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabReceitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabReceitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sc/senai/futuro/dev/tubarao/lab/receitas/LabReceitasApplicationTests.java
@@ -0,0 +1,13 @@
+package sc.senai.futuro.dev.tubarao.lab.receitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabReceitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
