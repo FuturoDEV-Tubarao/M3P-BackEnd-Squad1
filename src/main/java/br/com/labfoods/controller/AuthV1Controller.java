2bbc3cdc1836041b3caa0ac466e15e0b9d17a296
Nathalia Lanzendorf <nathalia.lanzendorf@gmail.com>
2024-07-14 17:30:45 -0300

    Ajustado endpoint de autenticação sem parametros

--- src/main/java/br/com/labfoods/controller/AuthV1Controller.java
+++ src/main/java/br/com/labfoods/controller/AuthV1Controller.java
@@ -3,30 +3,27 @@ package br.com.labfoods.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import br.com.labfoods.config.security.JwtService;
-import br.com.labfoods.dto.LoginV1Dto;
 import br.com.labfoods.dto.SessionV1Dto;
+import br.com.labfoods.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/labfoods/v1/auth")
 public class AuthV1Controller {
 
-    private JwtService service;
+    private AuthService service;
 
     @Autowired
-    public AuthV1Controller(JwtService service) {
+    public AuthV1Controller(AuthService service) {
         this.service = service;
     }
 
     @PostMapping()
     @Operation(summary = "Generate a session.", tags = "AuthV1Controller")
-    public ResponseEntity<SessionV1Dto> login(@RequestBody @Valid LoginV1Dto dto) {
-        String jwt = service.generateToken(dto.getEmail());
+    public ResponseEntity<SessionV1Dto> login() {
+        String jwt = service.generateToken();
         
         SessionV1Dto sessionDto = SessionV1Dto.builder().token(jwt).build();
 
