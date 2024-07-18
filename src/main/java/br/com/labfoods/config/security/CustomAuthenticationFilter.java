7ec937155430a1d8c672c99eff83bdc841db7d71
Nathalia Lanzendorf <nathalia.lanzendorf@gmail.com>
2024-07-18 20:03:30 -0300

    Corrigido import

--- src/main/java/br/com/labfoods/config/security/CustomAuthenticationFilter.java
+++ src/main/java/br/com/labfoods/config/security/CustomAuthenticationFilter.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import br.com.labfoods.config.service.BasicService;
 import br.com.labfoods.config.service.JwtService;
 import br.com.labfoods.utils.exceptions.UnauthorizedException;
